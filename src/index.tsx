5ba728d1453df789d3007b83e404a71cdd1cfc43
5aava <alex@creoplay.ru>
2020-07-19 18:33:53 +0300

    feat init

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+import App from "./components/App";
+
+import { BrowserRouter } from 'react-router-dom';
+
+
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root"),
+);
